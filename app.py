d1dcebf77107d31e47ad65ae5ca408a8bcb4b069
ckim48 <ckim48@u.rochester.edu>
2023-10-18 18:52:49 +0900

    sss

--- app.py
+++ app.py
@@ -316,11 +316,24 @@ def library():
     if request.method == "GET":
         # Obtain book info
         myBooks = db.execute("SELECT isbn, title, authors, cover FROM library")
+        # Create a dictionary to store book ratings by ISBN
+        book_ratings = {}
 
+        # Fetch and store the rating for each book
+        for book in myBooks:
+            isbn = book['isbn']
+            # Fetch the rating for the book
+            rating = db.execute("SELECT rating FROM rating WHERE isbn = ? AND username = ?", isbn,session["user_id"])
+            # If a rating exists, store it in the dictionary
+            if rating:
+                book_ratings[isbn] = int(rating[0]["rating"])
+            else:
+                # If no rating exists, set it to 0
+                book_ratings[isbn] = 0
         # Obtain emails to allow users to contact the book owners
         emails = db.execute("SELECT user_email FROM library")
 
-        return render_template("library.html", books=myBooks, emails=emails)
+        return render_template("library.html", books=myBooks, emails=emails,book_ratings = book_ratings)
 
     else:
         # If request via POST (user clicked on details button for a specific book)
